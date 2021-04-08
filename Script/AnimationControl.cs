ae2baf67b38bb546374f28431d1f8abf48781726
notRealHawk <humayunmetadort@gmail.com>
2021-04-08 16:38:14 +0500

    CharacterSelection Update
    Added new animations and Blend Tree

---
+++ Script/AnimationControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationControl : MonoBehaviour
+{
+    public Animator anim;
+    float valueChange = 0;
+    public float increament;
+    bool increase, decrease;
+    void Start(){
+        anim.SetFloat("AnimationBlend", 0);
+    }
+
+    void Update()
+    {
+        if(valueChange <= 0){
+            increase = true;
+            decrease = false;
+        }
+        if(valueChange >= 1){
+            decrease = true;
+            increase = false;
+        }
+        if(increase)
+        {
+            valueChange += increament;
+            anim.SetFloat("AnimationBlend", valueChange);
+        }
+        if(decrease)
+        {
+            valueChange -= increament;
+            anim.SetFloat("AnimationBlend", valueChange);
+        }
+    }
+}
